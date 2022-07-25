fb3ab01ec1cc4d7395fadc369b8273ede5fc5d44
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2022-07-25 10:58:06 +0100

    about us responsive

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -5,6 +5,9 @@ import Main from './components/Main/Main';
 import Loading from '../../components/Loading/Loading';
 import Story from './components/Story/Story';
 import Whyus from './components/Whyus/Whyus';
+import Quote from '../Register/components/Main/Quote'
+import Cover from '../../assets/img/cover_quote.svg'
+import Community from '../Home/components/Community/Community'
 // import Promise from './components/Promise/Promise';
 
 const About = () => {
@@ -28,6 +31,8 @@ const About = () => {
       <Story />
       <Whyus />
       {/* <Promise /> */}
+      <Quote style={{marginTop: "290px"}} widthSize="100%" heightSize="720px" title="Our Promise To You" par="“quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing”" img={Cover} titleMargin="140px" contMargin="240px" parWidth="50%" infoWidth="14%" topMargin="200px"/>
+      <Community />
       <Footer />
     </>
   );
