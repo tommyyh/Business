740f995ffab97f0206243fcf413947fb7c0261cf
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2022-07-27 17:22:30 +0100

    Login page laptop designed (form2 class left unhidden)

--- client/src/pages/Login/components/Main/Main.js
+++ client/src/pages/Login/components/Main/Main.js
@@ -10,6 +10,8 @@ import {
   setSurname,
   login,
 } from '../../../../features/user/userSlice';
+import Quote from '../../../Register/components/Main/Quote'
+import BG from '../../../../assets/img/right_bg.svg'
 
 const Main = () => {
   const [error, setError] = useState('');
@@ -44,11 +46,9 @@ const Main = () => {
   };
 
   return (
+
     <main className={css.main_cont}>
       <div className={css.main}>
-        <h1>Login page</h1>
-        <p>lacus sed turpis tincidunt id aliquet risus feugiat in ante </p>
-
         <div className={css[errorClass]}>
           <p>{error}</p>
 
@@ -57,32 +57,42 @@ const Main = () => {
             onClick={() => setErrorClass('error_closed')}
           ></div>
         </div>
+        <div className={css.whole_section}>
+          <div className={css.left_section}>
+            <h1>Login page</h1>
+            <p>lacus sed turpis tincidunt id aliquet risus feugiat in ante </p>
 
-        <div className={css.form1}>
-          <div className={css.input_email}>
-            <label>Email Address</label>
-            <input
-              type='text'
-              placeholder='address@gmail.com'
-              onChange={(e) => setValues({ ...values, email: e.target.value })}
-              value={values.email}
-            />
+            <div className={`${css.form1} ${css.form_cont}`}>
+              <div className={`${css.input_email} ${css.form}`}>
+                <label className={`${css.label}`}>Email Address</label>
+                <input
+                  className={`${css.email}`}
+                  type='text'
+                  placeholder='address@gmail.com'
+                  onChange={(e) => setValues({ ...values, email: e.target.value })}
+                  value={values.email}
+                />
+              </div>
+
+              <div className={`${css.input_password} ${css.form}`}>
+                <label className={`${css.label}`}>Password</label>
+                <input
+                  className={`${css.password}`}
+                  type='password'
+                  placeholder='Your Password'
+                  onChange={(e) =>
+                    setValues({ ...values, password: e.target.value })
+                  }
+                  value={values.password}
+                />
+              </div>
+
+              <button className={`${css.loginBtn  } ${css.form_btn}`} onClick={submit}>Login</button>
+            </div>
           </div>
 
-          <div className={css.input_password}>
-            <label>Password</label>
-            <input
-              type='password'
-              placeholder='Your Password'
-              onChange={(e) =>
-                setValues({ ...values, password: e.target.value })
-              }
-              value={values.password}
-            />
-          </div>
+          <Quote widthSize="640px" heightSize="620px" title="Bond With people" par="“quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing”" img={BG} titleMargin="104px" contMargin="180px" parWidth="80%" infoWidth="30%"/>
         </div>
-
-        <button onClick={submit}>Login</button>
       </div>
     </main>
   );
