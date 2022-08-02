dc065972f97be82e5854fff19b38f1aa0e8ab1ed
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2022-08-02 09:33:44 +0100

    authentication quote section aligned

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -5,7 +5,7 @@ import Main from './components/Main/Main';
 import Loading from '../../components/Loading/Loading';
 import Story from './components/Story/Story';
 import Whyus from './components/Whyus/Whyus';
-import Quote from '../Register/components/Main/Quote'
+import Quote from './components/Main/Quote'
 import Cover from '../../assets/img/cover_quote.svg'
 import Community from '../Home/components/Community/Community'
 // import Promise from './components/Promise/Promise';
